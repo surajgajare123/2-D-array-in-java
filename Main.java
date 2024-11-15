2bc3a29689571d0d7abefd878722d6616f60f826
Suraj <108755893+surajgajare123@users.noreply.github.com>
2024-11-15 20:11:09 +0530

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,22 @@
+class Main{
+    public static void main(String[] args){
+        System.out.println("Multidimensional 2-D array!");
+        int [][] vector = {{12, 34, 67, 89},
+                           {12, 35, 78, 37},
+                           {15, 54, 23, 77}};
+    
+        for(int i = 0; i < vector.length; i++){
+            for(int j = 0; j < vector[i].length; j++){
+            System.out.print(vector[i][j]);
+            System.out.print(" ");
+        }
+        System.out.println("");
+    }
+     System.out.println("Multidimensional 3-D array !");
+     int[][][] marks = {
+         {11, 22, 33, 44},
+         {111, 222, 333, 444},
+         {1111, 2222, 3333, 4444
+     }
+}
+}
